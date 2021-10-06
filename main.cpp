0d3d229277c1466cad3d77a9c38ffde8a841ba64
Asif Islam <asif@asifislam.dev>
2021-10-06 17:32:01 -0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include "StringData.h"
+
+int main() {
+    int linearSearch(std::vector<std::string> datatSet, std::string element);
+    int binarySearch(std::vector<std::string> datatSet, std::string element);
+    std::string message = "not_here";
+
+    long start = systemTimeNanoseconds();
+    std::cout << "\nBinary Search Time: ";
+    binarySearch(getStringData(), message);
+    long end = systemTimeNanoseconds();
+    std::cout << end-start;
+    start = systemTimeNanoseconds();
+    std::cout << "\nLinear Search Time: ";
+    linearSearch(getStringData(), message);
+    end = systemTimeNanoseconds();
+    std::cout << end-start;
+
+    message = "mzzzz";
+    start = systemTimeNanoseconds();
+    std::cout << "\nBinary Search Time: ";
+    binarySearch(getStringData(), message);
+    end = systemTimeNanoseconds();
+    std::cout << end-start;
+    start = systemTimeNanoseconds();
+    std::cout << "\nLinear Search Time: ";
+    linearSearch(getStringData(), message);
+    end = systemTimeNanoseconds();
+    std::cout << end-start;
+
+    message = "aaaaa";
+    start = systemTimeNanoseconds();
+    std::cout << "\nBinary Search Time: ";
+    binarySearch(getStringData(), message);
+    end = systemTimeNanoseconds();
+    std::cout << end-start;
+    start = systemTimeNanoseconds();
+    std::cout << "\nLinear Search Time: ";
+    linearSearch(getStringData(), message);
+    end = systemTimeNanoseconds();
+    std::cout << end-start;
+
+    return 0;
+}
+
+
+int linearSearch(std::vector<std::string> dataSet, std::string element) {
+    for (int i = 0; i < dataSet.size(); i++) {
+        if (dataSet[i] == element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(std::vector<std::string> datatSet, std::string element) {
+    int low,mid,high;
+    high = datatSet.size()-1;
+    low = 0;
+
+    while (high >= low) {
+        mid = (low + (high-low)/2);
+        if (element > datatSet[mid]) {
+            low = mid + 1;
+        } else if (element < datatSet[mid]) {
+            high = mid - 1;
+        } else if (element == datatSet[mid]) {
+            return mid;
+        }
+    }
+
+    return -1;
+}
